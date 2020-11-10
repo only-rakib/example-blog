e36b8942688139a8b1ff0f659629c9082242d459
Rakib <rakibulh170@gmail.com>
2020-11-11 03:21:28 +0600

    registration with error

--- authentication/views.py
+++ authentication/views.py
@@ -1,25 +1,47 @@
 from django.http import Http404
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from json import loads as jsonloads
 from django.http import HttpResponseRedirect
 from . models import User
 # LOGIN VIEW ENDPOINT
+
 
 def login(response):
 
     return render(response, 'login.html')
 
+
 @csrf_exempt
 def register(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        password = request.POST['pass2']
-        user = User.objects.create_user(first_name=first_name,last_name=last_name,password=password,email=email)
-        user.save()
-        return redirect("posts:post_home")
+
+        try:
+            #datas = jsonloads(request.POST["val"])
+            # print(datas)
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            password = request.POST['pass2']
+            # first_name=datas['first_name']
+            #last_name = datas['last_name']
+            #email =datas['email']
+            #password =datas['pass2']
+            # print(first_name)
+            user = User.objects.create_user(
+                first_name=first_name, last_name=last_name, password=password, email=email)
+            user.save()
+            y={'mgs':'success'}
+            return render(request, 'register.html',{'x':y})
+            #return redirect("posts:post_home",{'x':y})
+            # return JsonResponse({}, status=200)
+        except Exception as ex:
+            y={'mgs':'failed',
+                'error':ex,
+            }
+            
+            return render(request, 'register.html',{'x':y})
+            #return redirect("posts:post_home",{'x':y})
     return render(request, 'register.html')
